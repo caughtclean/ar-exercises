26744b5e048d6ab85da9c220676fe3e0bb97932f
Nathan Eyerley <eyerleyn@gmail.com>
2016-12-06 22:58:54 +0000

    finished first 6 exercises

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -7,4 +7,12 @@ require_relative './exercise_4'
 puts "Exercise 5"
 puts "----------"
 
-# Your code goes here ...
+pp Store.sum(:annual_revenue)
+pp Store.average(:annual_revenue).to_s
+stores = Store.all
+pp Store.where("annual_revenue > 1000000").count
+
+
+
+
+
--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -8,4 +8,19 @@ require_relative './exercise_5'
 puts "Exercise 6"
 puts "----------"
 
-# Your code goes here ...
+class Store < ActiveRecord::Base
+  has_many :employees
+end
+
+class Employee < ActiveRecord::Base
+  belongs_to :store
+end
+
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Nathan", last_name: "Eyerley", hourly_rate: 85)
+@store2.employees.create(first_name: "Bob", last_name: "Hill", hourly_rate: 60)
+@store2.employees.create(first_name: "Bill", last_name: "Hank", hourly_rate: 60)
+@store2.employees.create(first_name: "Joe", last_name: "Son", hourly_rate: 60)
+
+pp Store.all
+pp Employee.all
